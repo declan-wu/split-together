c7911f72d27a36be41dbd8b87b33469c922a4411
Declan Wu <declan.s.wu@gmail.com>
2019-10-11 22:05:46 -0400

    uncommented signout page

--- src/pages/home/homepage.component.jsx
+++ src/pages/home/homepage.component.jsx
@@ -1,40 +1,43 @@
-import React, { useContext } from 'react';
-import { Redirect } from 'react-router-dom';
-import Button from '../../components/Button/button.component';
-import firebase from '../../firebase/firebase.utils';
-import { AuthContext } from '../../firebase/auth.context';
+import React, { useContext } from "react";
+import { Redirect } from "react-router-dom";
+import Button from "../../components/Button/button.component";
+import firebase from "../../firebase/firebase.utils";
+import { AuthContext } from "../../firebase/auth.context";
 
-import ButtonRedirect from '../../components/RedirectButton/button-redirect.component';
-import Logo from './37c04452-e6e3-40b8-bfd1-407ea1ff9047_200x200.png';
+import ButtonRedirect from "../../components/RedirectButton/button-redirect.component";
+import Logo from "./37c04452-e6e3-40b8-bfd1-407ea1ff9047_200x200.png";
 
-import './homepage.style.css';
+import "./homepage.style.css";
 
 const HomePage = () => {
   const { currentUser } = useContext(AuthContext);
 
+  // uncomment to prevent redirect
   if (currentUser) {
     return <Redirect to="/main" />;
   }
 
   return (
-    <div className={'home__background'}>
-      <div className={'home__layout'}>
+    <div className={"home__background"}>
+      <div className={"home__layout"}>
         <img src={Logo} alt="Logo" />
-        <ButtonRedirect className={'home__button'} route={`/login`}>
+        <ButtonRedirect className={"home__button"} route={`/login`}>
           Log in
         </ButtonRedirect>
         <ButtonRedirect
-          className={'home__button home__button--bot'}
+          className={"home__button home__button--bot"}
           route={`/signup`}
         >
           Sign up
         </ButtonRedirect>
-        {/* <Button
-          className={'profile_header__signout'}
+        {/*
+        <Button
+          className={"profile_header__signout"}
           onClick={() => firebase.auth().signOut()}
         >
           Sign out
-        </Button> */}
+        </Button>
+        */}
       </div>
     </div>
   );
